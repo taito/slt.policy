f82632f5bcbc3a942a5d3a4bc0cd546eb6e696e9
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-01-16 10:37:45 +0200

    Release 0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='slt.policy',
-    version='0.5',
+    version='0.6',
     description="Turns plone site into SLT shopping site.",
     long_description=open("README.rst").read(),
     classifiers=[
